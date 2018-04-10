ec96916683a8a328bf5a30ea17319fe7f1787875
Aditya Agarwal <adityaa803@gmail.com>
2018-04-10 15:11:12 +0530

    fixes #1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'mini.css/dist/mini-default.min.css';
+import './assets/style.css';
 import Vue from 'vue';
 import App from './App';
 
